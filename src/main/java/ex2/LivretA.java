292f7efb0e2b8a00475addf81fb0b67ee1c0217f
johan guillen <sioupes34@gmail.com>
2024-10-29 11:15:01 +0100

    ex2 finis

--- src/main/java/ex2/LivretA.java
+++ src/main/java/ex2/LivretA.java
@@ -2,8 +2,39 @@ package ex2;
 
 public class LivretA extends CompteBancaire {
 
-	public LivretA(String type, double solde, double tauxRemuneration) {
-		super(type, solde, 0, tauxRemuneration);
+
+	/** tauxRemuneration : taux de rémunération*/
+	private double tauxRemuneration;
+
+
+	/** constructeur d'un livret A
+	 * @param solde
+	 * @param tauxRemuneration
+
+	 */
+	public LivretA(double solde, double tauxRemuneration) {
+		super(solde,0);
+		this.tauxRemuneration=tauxRemuneration;
+
 	}
 
+
+	/** Applique la remuneration annuel du livret A
+	 */
+	public void appliquerRemuAnnuelle(){
+
+			setSolde(getSolde()*this.tauxRemuneration/100);
+		}
+	/** Getter for tauxRemuneration
+	 * @return the tauxRemuneration
+	 */
+	public double getTauxRemuneration() {
+		return tauxRemuneration;
+	}
+	/** Setter
+	 * @param tauxRemuneration the tauxRemuneration to set
+	 */
+	public void setTauxRemuneration(double tauxRemuneration) {
+		this.tauxRemuneration = tauxRemuneration;
+	}
 }
