039eea55db19dca2061d07eb47ff9b788b27df43
johan guillen <sioupes34@gmail.com>
2024-10-29 10:50:53 +0100

    ex1 finis

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,26 @@
+package ex1;
+
+/** classe qui propose des opérations sur les nombres
+ *
+ */
+public class Calcul {
+
+	/** retourne l'addition de 2 entiers
+	 *
+	 * @param a entier 1
+	 * @param b entier 2
+	 * @return int
+	 */
+	public int addition(int a, int b){
+		return a+b;
+	}
+	/** retourne la soustraction de 2 entiers
+	 *
+	 * @param a entier 1
+	 * @param b entier 2
+	 * @return int
+	 */
+	public int soustraction(int a, int b){
+		return a-b;
+	}
+}
---
+++ src/main/java/ex1/Entreprise.java
@@ -0,0 +1,38 @@
+package ex1;
+
+import java.util.Date;
+
+/**
+ * Classe d'une entreprise
+ */
+public class Entreprise {
+	/**
+	 * le siret en int de l'entreprise
+	 */
+	private int siret;
+	/**
+	 * le no en string de l'entreprise
+	 */
+	private String nom;
+	/**
+	 * l'adresse en string de l'entreprise
+	 */
+	private String adresse;
+	/**
+	 * la date de creation en Date de l'entreprise
+	 */
+	private Date dateCreation;
+	/**
+	 * le capital maximum de l'entreprise en int
+	 * variable constante
+	 */
+	public static final int CAPITALMAX = 3000000;
+	/**
+	 * Permet d'afficher le statut de l'entreprise
+	 * pas d'argument d'entrée et de sortie
+	 */
+	public void afficherStatut(){
+		
+	}
+	
+}
